90cce2d111099a2b8618ceab17127c9bf2a41bfd
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-11-18 22:20:09 -0600

    use final fixes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,40 +1,13 @@
-local function replace_ingredient(ingredients, old_ingredient, new_ingredient)
-    for i, ingredient in pairs(ingredients) do
-        if ingredient.name then
-            if ingredient.name == old_ingredient then
-                ingredient.name = new_ingredient
-            end
+log("Total items in data.raw[item]: " .. #data.raw["item"])
+
+local categories = {"item", "tool", "ammo", "capsule"}
+for _, category in pairs(categories) do
+    if data.raw[category] then
+        for name, item in pairs(data.raw[category]) do
+            log(category .. ": " .. name)
         end
     end
 end
-
-local function replace_results(results, old_result, new_result)
-    for i, result in pairs(results) do
-        if result.name then
-            if result.name == old_result then
-                result.name = new_result
-            end
-        end
-    end
-end
-
-local function update_recipe(recipe, old_item, new_item)
-    if data.raw.recipe[recipe] and data.raw.recipe[recipe].ingredients then
-        replace_ingredient(data.raw.recipe[recipe].ingredients, old_item, new_item)
-    end
-
-    if data.raw.recipe[recipe .. "-recycling"] and data.raw.recipe[recipe .. "-recycling"].results then
-        replace_results(data.raw.recipe[recipe .. "-recycling"].results, old_item, new_item)
-    end
-end
-
-update_recipe("concrete", "iron-ore", "iron-stick")
-
-if mods["Dectorio"] then
-    update_recipe("dect-concrete-grid", "iron-ore", "iron-stick")
-end
-
--- My code below
 
 local production_and_logistics_items = {}
 
@@ -43,6 +16,8 @@ for name, item in pairs(data.raw["item"]) do
         table.insert(production_and_logistics_items, item)
     end
 end
+
+log("Items: " .. #production_and_logistics_items)
 
 local function is_raw_material(ingredient_name)
     local raw_materials = {
@@ -176,7 +151,7 @@ for _, item in pairs(production_and_logistics_items) do
         if winner then
             local total_raw_cost = calculate_total_raw_cost(recipe)
             local winner_raw_cost = calculate_raw_material_cost(winner.name, 1)
-            local scaling_factor = total_raw_cost / winner_raw_cost
+            local scaling_factor = total_raw_cost / calculate_raw_material_cost(winner.name, recipe.results[1].amount)            
             local scaled_quantity = math.ceil(scaling_factor)
 
             modify_recipe(recipe, winner, scaled_quantity)
